55f682c734a369f8af0ace7c9f07a017cb7c0db8
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-08-16 09:38:36 +0200

    track current tests

---
+++ js/gw.js
@@ -0,0 +1,12 @@
+gw.mainObject = function(width, height, canvas = null) {
+  this.width = width;
+  this.height = height;
+
+  if (canvas != null) {
+    this.canvas = 
+  }
+}
+
+gw.mainObject.prototype.setWidth(width) {
+  this.width = width;
+}

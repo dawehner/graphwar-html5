b7907ca5808b4bf3a37edba77f28013f3791ad36
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-08-16 10:40:15 +0200

    some update

--- js/gw.js
+++ js/gw.js
@@ -1,12 +1,21 @@
-gw.mainObject = function(width, height, canvas = null) {
+var gw = gw || {};
+
+
+gw.mainObject = function(width, height) {
   this.width = width;
   this.height = height;
 
-  if (canvas != null) {
-    this.canvas = 
-  }
-}
+  this.canvas = new fabric.Canvas('gw-canvas');
+};
 
-gw.mainObject.prototype.setWidth(width) {
+gw.mainObject.prototype.setWidth = function(width) {
   this.width = width;
-}
+};
+
+gw.player = function(name, icon = "", color = "") {
+  
+};
+
+gw.shooter = function(player) {
+  this.player = player;
+};
